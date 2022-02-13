48632c47e1b0af19d98356a3d219e546bf582120
Sujata <58798972+Sujata658@users.noreply.github.com>
2022-02-13 22:07:54 +0545

    SujataCode1

---
+++ src/src/js/fixture.js
@@ -0,0 +1,47 @@
+//import {teamList} from "./array"
+
+const a=["A","B","C","D"];
+//for array length
+var n=a.length,b=1;
+var list = "";
+ 
+//rounds
+function match(){
+    //for fixture
+    do{
+        if(b==n){
+            if(n%2==0){
+                break;
+            }
+        }
+        console.log(b);
+        for(let i=0; i<a.length/2; i++){
+            n = n-1;
+            if(i!=n)
+            {
+                list += a[i] + "-" + a[n] + "<br>";
+                document.getElementById("matched_teams").innerHTML = list;
+            }
+        }
+        console.log(list);
+        b++;
+        n = a.length;
+        re_order();
+    }while(b<=n);
+}
+function re_order(){
+    var b=1;
+    for(let i=0;i<n-1;i++)
+    {   
+        if(b==n-1){
+            if(n%2==0){
+                break;
+            }
+        }
+        var temp;
+        temp=a[i];
+        a[i]=a[i+1];
+        a[i+1]=temp;
+        b++;
+    }
+}
