9bac14cf34377f0bc383311ba87f46eb455c16b0
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-03-01 13:46:14 +0545

    final

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,14 +1,46 @@
-let bubbleSort = (inputArr) => {
-
+function bubbleSortByWon(inputArr) {
+           
     let len = inputArr.length;
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j].points > inputArr[j + 1].points) {
-                let tmp = inputArr[j].points;
-                inputArr[j].points = inputArr[j + 1].points;
-                inputArr[j + 1].points = tmp;
+        for (let j =0; j < len-i-1; j++) {
+            if (inputArr[j].won > inputArr[j+1].won) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j+1];
+                inputArr[j+1] = tmp;
+                
             }
         }
     }
+   inputArr.reverse();
+    return inputArr;
+};
+function bubbleSortByGoals(inputArr) {
+
+    let len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (inputArr[j].goals > inputArr[j + 1].goals) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+    inputArr.reverse();
+    return inputArr;
+};
+function bubbleSortByGoals(inputArr) {
+
+    let len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (inputArr[j].goals > inputArr[j + 1].goals) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+    inputArr.reverse();
     return inputArr;
 };
